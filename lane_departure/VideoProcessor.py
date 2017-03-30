700d1e58eb0fd453e202da217c3d2de82577dddb
Michael Munoz <mmunoz779@outlook.com>
2017-03-30 12:54:27 -0400

    Add files via upload

--- lane_departure/VideoProcessor.py
+++ lane_departure/VideoProcessor.py
@@ -6,47 +6,7 @@ from Sensor import LaneSensor
 from LineDetector import LineDetector
 import sys
 
-def facial():
-    eyeCascade = cv2.CascadeClassifier('C:\opencv\sources\data\haarcascades_cuda\haarcascade_frontalface_default.xml')
-    faceCascade = cv2.CascadeClassifier('C:\opencv\sources\data\haarcascades_cuda\haarcascade_eye.xml')
-    video_capture = cv2.VideoCapture(0)
-    
-    while True:
-        ret, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.rectangle(frame, (150, 50), (500, 400), (0, 0, 255), 2)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor= 1.1,
-            minNeighbors=5,
-            minSize=(30,30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-        
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y), (x+w, y+h), (255, 0, 0), 2)
-            
-        eyes = eyeCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30,30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-        
-        
-        for (x,y,w,h) in eyes:
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-            
-        cv2.imshow('Video', frame)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-            
-    video_capture.release()
-    cv2.destroyAllWindows()
 #Initialize video input
-facial()
 #stream = cv2.VideoCapture(0) #6 7 8
 stream = cv2.VideoCapture('C:/Users/michael/OneDrive/Visteon/ldwNight.avi') #6 7 8
 if stream.isOpened() == False:
@@ -117,5 +77,3 @@ while(cv2.waitKey(1) != 27):
     videoWriter.write(outputFull)
     
 cv2.destroyAllWindows()
-
-
