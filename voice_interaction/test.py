3830c863486de7180b8ccba8768ef6a2f87d03ae
Allison Hurley <allison@hurleyhome.com>
2017-05-05 21:52:28 +0000

    Fixed the pull down for tts in test.

--- voice_interaction/test.py
+++ voice_interaction/test.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(11, GPIO.IN)
+GPIO.setup(13, GPIO.IN)
 
 from subprocess import call
 
